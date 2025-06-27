93f17a607c1b4e8920009c14148f5ae2490d7730
Jayesh <jayeshgavale2000@gmail.com>
2025-06-27 11:45:31 +0530

    first commit

---
+++ Spring-IOC-Container-And-Beans-Creation-Using-XML/src/main/java/com/tka/Student.java
@@ -0,0 +1,41 @@
+package com.tka;
+
+public class Student {
+
+	
+	private String name;
+	private int age;
+	private String city;
+	public Student() {
+		
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public Student(String name, int age, String city) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.city = city;
+	}
+	@Override
+	public String toString() {
+		return "Student [name=" + name + ", age=" + age + ", city=" + city + "]";
+	}
+
+}
---
+++ Spring-IOC-Container-And-Beans-Creation-Using-XML/src/main/java/com/tka/home.java
@@ -0,0 +1,17 @@
+package com.tka;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class home {
+
+	public static void main(String[] args) {
+
+	ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+	
+	Student s = context.getBean(Student.class);
+	System.out.println(s);
+	
+		
+	}
+
+}
